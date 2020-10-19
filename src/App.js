d20e2efedc5147180347018381325b7475b4cbc3
yasirshahid <yasirshahid5@gmail.com>
2020-10-19 13:20:15 +0500

    add textInput

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import TextInput from "./components/TextInput"
 import './App.css';
 
 function App() {
   return (
     <div >
       <h3>Hello world its a react props practice</h3>
+      <TextInput/>
     </div>
   );
 }
