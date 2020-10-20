b02b9a472f333190072b817431c212fd0dd8cfe4
yasirshahid <yasirshahid5@gmail.com>
2020-10-20 13:08:53 +0500

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Text from "./components/TextInput"
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false);
+
   return (
-    <div >
+    <div className={`box ${isMorning ? "dayLight" : ""}`}>
+      <h2>Hello {isMorning ? "Morning" : "Night"}</h2>
+      <button onClick={()=> setMorning(!isMorning)}>day Change</button>
+      <h3>This is a counter {count}</h3>
+      <button onClick={() => setCount(++count)}>Update</button>
       <h3>Hello world its a react props practice</h3>
-      <Text day="Tuesday"/>
-      <Text day="Monday"/>
+      <Text day="Tuesday" />
+      <Text day="Monday" />
     </div>
   );
 }
